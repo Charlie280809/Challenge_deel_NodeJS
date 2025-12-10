b0629b2533f92c3068e990d2e79602d89565074c
Charlie Van Belle <r0995521@student.thomasmore.be>
2025-12-10 14:08:08 +0100

    add API routes

--- index.js
+++ index.js
@@ -12,7 +12,43 @@ app.use(express.json());
 
 //MongoDB verbinding
 mongoose
-    .connect(process.env.MONGODB_URI, {useNewUrlParser: true })
+    .connect(process.env.MONGODB_URL, {useNewUrlParser: true })
     .then(() => { console.log("MongoDB verbonden"); })
     .catch((err) => { console.error("MongoDB verbindingsfout:", err); });
 
+
+//Alle bags ophalen
+app.get('/bag', async (req, res) => {
+    try {
+        const items = await Config.find();
+        res.json(items);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+//Nieuwe bag toevoegen
+app.post('/bag', async (req, res) => {
+    try{
+        const nieuwItem = await Config.create(req.body);
+        res.json(nieuwItem);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+//Bag verwijderen op id
+app.delete('/bag/:id', async (req, res) => {
+    try {
+        const verwijderdItem = await Config.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Item verwijderd', item: verwijderdItem });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+    
+
+app.listen(port, () => {
+    console.log(`Server draait op poort ${port}`);
+});
